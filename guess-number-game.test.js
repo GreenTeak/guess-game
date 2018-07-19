7c7c41b237815d73afa65f8b55eda9b79e65face
Tian Ge <greenteak1992@gmail.com>
2018-07-19 20:23:42 +0800

    output is XAXB

--- guess-number-game.test.js
+++ guess-number-game.test.js
@@ -5,6 +5,16 @@ describe("GuessNumberGame",function () {
         expect(new GuessNumberGame().test("1 2 3 4")).toEqual("4A0B")
 
     });
-    
+    it('should return 3A0B for input 1 2 3 5 when answer is 1 2 3 4',function () {
+        expect(new GuessNumberGame().test("1 2 3 5")).toEqual("3A0B");
+    })
+    it('should return 0A4B for input 4 3 2 1 when answer is 1 2 3 4',function () {
+        expect(new GuessNumberGame().test("4 3 2 1")).toEqual("0A4B");
+
+    })
+    it('should return 2A2B for input 1 2 4 3 when answer is 1 2 3 4',function () {
+        expect(new GuessNumberGame().test("1 2 4 3")).toEqual("2A2B");
+
+    })
 
 });
--- guess-number-guess.js
+++ guess-number-guess.js
@@ -1,9 +1,36 @@
 class GuessNumberGame {
-    constructor(){
 
+    constructor(){
+        this.answer = "1 2 3 4";
     }
+    PostionIsTure(Input) {
+        let Anumber = 0;
+        let input = Input.split(" ");
+        const answers = this.answer.split(" ");
+
+        for (let i = 0; i < 4; i++){
+            if(answers[i] == input[i]) {
+                Anumber++;
+            }
+        }
+        return Anumber;
+    }
+    numberIsTure(input){
+        let numberB = 0;
+        let inputs = input.split(" ");
+        const answers = this.answer.split(" ");
+
+        for (let i = 0; i < 4; i++){
+            if(answers.includes(inputs[i])){
+                numberB ++;
+            }
+        }
+        return numberB;
+    }
+
     test(input){
-        return '4A0B';
+        const numberA = this.PostionIsTure(input);
+        return `${numberA}A${this.numberIsTure(input) - numberA}B`;
     }
 
 }
